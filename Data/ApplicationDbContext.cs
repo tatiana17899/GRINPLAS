996f63d5e5ed9b8609f3f157329072d4775f1ef1
Julio3021 <julio_antaurco@usmp.pe>
2025-04-01 23:35:11 -0500

    inicio del proyecto

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GRINPLAS.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
