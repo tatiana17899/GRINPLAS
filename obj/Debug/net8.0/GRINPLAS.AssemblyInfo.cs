0a20660cd18a84956fd0296ffc939b2f7e66f5ce
tatiana17899 <tatianasuarezrosas332@gmail.com>
2025-04-10 23:17:17 -0500

    aaa

--- obj/Debug/net8.0/GRINPLAS.AssemblyInfo.cs
+++ obj/Debug/net8.0/GRINPLAS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GRINPLAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+177398a0f23ad888117af1f1826ee7e5f03155d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a06757f58ce4e815fd57ca497e37061e55fc00f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GRINPLAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GRINPLAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
