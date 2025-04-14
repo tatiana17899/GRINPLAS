fb60a8a3e7be22e13948588d3e67650021b82cf8
Andrea <camposhuaman1276@gmail.com>
2025-04-14 21:18:25 +0000

    TerminosyCondiciones

--- Controllers/InicioAdmiController.cs
+++ Controllers/InicioAdmiController.cs
@@ -22,6 +22,10 @@ namespace GRINPLAS.Controllers
         {
             return View();
         }
+         public IActionResult TyC()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
