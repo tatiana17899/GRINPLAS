98b2d5b91ad2c1bad6ba80afcc2f2a3954d7dcfd
tatiana17899 <tatianasuarezrosas332@gmail.com>
2025-05-21 18:58:11 -0500

    front end restante

--- wwwroot/js/carrusel.js
+++ wwwroot/js/carrusel.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const hero = document.querySelector(".hero-content");
+  const hero = document.querySelector(".header");
   const sliderIcons = document.querySelectorAll(".slider i");
   let currentSlide = 0;
 
   // Array de imágenes de fondo
   const images = [
-    "https://images.unsplash.com/photo-1562280963-8a5475740a42?q=80&w=2069&auto=format&fit=crop",
-    "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?q=80&w=2070&auto=format&fit=crop",
-    "https://images.unsplash.com/photo-1565814636199-ae8133055c1c?q=80&w=2070&auto=format&fit=crop",
+    "https://i.ibb.co/mry5W1tX/b31edadeb493ee7e5858827193910ed0.jpg",
+    "https://i.ibb.co/sdyYs29Y/bolsas-de-plastico-de-colores-dispuestos-sobre-fondo-gris.jpg",
+    "https://i.ibb.co/dwjTXWGF/rollos-de-bolsas-de-basura-en-espacio-de-fondo-blanco-para-el-texto.jpg",
   ];
 
   // Función para actualizar la imagen de fondo
   function updateSlide(index) {
-    hero.style.backgroundImage = `linear-gradient(rgba(112, 166, 127, 0.8), rgba(112, 166, 127, 0.8)), url(${images[index]})`;
+    hero.style.backgroundImage = `linear-gradient(rgba(0, 186, 167, 0.4), rgba(0, 186, 167, 0.4)),
+    url(${images[index]})`;
 
     // Actualizar estado activo de los íconos
     sliderIcons.forEach((icon, i) => {
