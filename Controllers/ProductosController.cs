47ba99612e71d9aefab87e479aa9b3194e1a144d
melivezla <131054190+melivezla@users.noreply.github.com>
2025-09-23 19:23:57 -0500

    funcionalidad del controlador de los productos

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -11,6 +11,8 @@ using GRINPLAS.ViewModel;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Build.Framework;
 
+
+
 namespace GRINPLAS.Controllers
 {
     public class ProductosController : Controller
@@ -63,6 +65,8 @@ namespace GRINPLAS.Controllers
                 Productos = await _context.Productos.Include(p => p.Categoria).ToListAsync(),
                 Categorias = await _context.Categorias.ToListAsync(),
             };
+
+
 
 
             return View(viewModel);
@@ -122,7 +126,7 @@ namespace GRINPLAS.Controllers
             {
                 if (producto.ProductoId == 0)
                 {
-                    // Inserción: no asignar ProductoId, que lo genere la BD
+                    // Inserción: no asignar ProductoId, que sea generada por la base de datos
                     _context.Add(producto);
                 }
                 else
@@ -136,7 +140,7 @@ namespace GRINPLAS.Controllers
                 return RedirectToAction("Administrador");
             }
 
-            // Si el modelo no es válido, recarga vista con datos actuales
+            // Si el modelo no es válido se recargará la vista con datos actuales
             var viewModel = new ProductoViewModel
             {
                 nuevoProducto = producto,
