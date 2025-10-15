ecfb01dad93050232ca06a42e4a747686f1bc3c3
melivezla <131054190+melivezla@users.noreply.github.com>
2025-10-15 17:22:38 -0500

    Update Gasto.cs

--- Models/Gasto.cs
+++ Models/Gasto.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
+//detalle de variables para gastos
 namespace GRINPLAS.Models
 {
     public class Gasto
